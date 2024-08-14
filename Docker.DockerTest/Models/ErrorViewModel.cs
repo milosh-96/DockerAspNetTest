f34d95425bbbd7fec844898582678d2a1beee22d
milosh-96 <milosjovanovic042@gmail.com>
2024-08-15 01:55:13 +0200

    Add project files.

---
+++ Docker.DockerTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Docker.DockerTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
